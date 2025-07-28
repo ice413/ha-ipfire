5786a2f88bd07214de2c9f84190393da773e109b
Claes Nilsson <claes.nilsson13@gmail.com>
2025-07-28 22:51:15 +0200

    fix: typo in init

--- custom_components/ipfire/__init__.py
+++ custom_components/ipfire/__init__.py
@@ -1,23 +1,24 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
+from homeassistant.exceptions import ConfigEntryNotReady
 from .const import DOMAIN
+from .coordinator import IPFireCoordinator
 
 PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up IPFire from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    try:
+        coordinator = IPFireCoordinator(hass, entry)
+        await coordinator.async_config_entry_first_refresh()
+    except Exception as err:
+        raise ConfigEntryNotReady(f"Failed to connect to IPFire: {err}") from err
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload an IPFire config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
 
